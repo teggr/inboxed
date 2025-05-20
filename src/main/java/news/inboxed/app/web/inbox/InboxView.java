16bf6cb10b50aa1abcfadbd6af424b78629aa934
Robin Tegg <robin@tegg.me.uk>
2025-05-20 12:27:10 +0000

    sort out action bar icons and dropdowns

--- src/main/java/news/inboxed/app/web/inbox/InboxView.java
+++ src/main/java/news/inboxed/app/web/inbox/InboxView.java
@@ -1,6 +1,9 @@
 package news.inboxed.app.web.inbox;
 
 import static j2html.TagCreator.*;
+import static j2html.TagCreator.h2;
+import static j2html.TagCreator.h5;
+import static j2html.TagCreator.nav;
 import static news.inboxed.app.web.site.Navigation.inboxedNavigation;
 
 import java.util.Map;
@@ -12,9 +15,14 @@ import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.AbstractView;
 
-import dev.rebelcraft.j2html.bootstrap.Bootstrap;
+import static dev.rebelcraft.j2html.bootstrap.Bootstrap.*;
+import static dev.rebelcraft.j2html.bootstrap.Bootstrap.col;
+import static dev.rebelcraft.j2html.bootstrap.Bootstrap.nav;
+
 import j2html.rendering.IndentedHtml;
 import j2html.tags.DomContent;
+import j2html.tags.specialized.HeaderTag;
+import j2html.tags.specialized.NavTag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import news.inboxed.app.inbox.InboxItem;
@@ -23,42 +31,181 @@ import news.inboxed.app.web.site.SiteLayout;
 @Component
 public class InboxView extends AbstractView {
 
-    @Override
-    @Nullable
-    public String getContentType() {
-        return MediaType.TEXT_HTML_VALUE;
-    }
+        @Override
+        @Nullable
+        public String getContentType() {
+                return MediaType.TEXT_HTML_VALUE;
+        }
 
-    @SuppressWarnings({ "unchecked", "null" })
-    @Override
-    protected void renderMergedOutputModel(
-        @Nullable Map<String, Object> model,
-        @NonNull HttpServletRequest request,
-        @NonNull HttpServletResponse response
-    ) throws Exception {
+        @SuppressWarnings({ "unchecked", "null" })
+        @Override
+        protected void renderMergedOutputModel(
+                        @Nullable Map<String, Object> model,
+                        @NonNull HttpServletRequest request,
+                        @NonNull HttpServletResponse response) throws Exception {
 
-        // get from the model
-        Page<InboxItem> inboxItems = (Page<InboxItem>) model.get("inboxItems");
-        String homeUrl = (String) model.get("homeUrl");
-        String searchUrl = (String) model.get("searchUrl");
-        String username = (String) model.get("username");
-        String logoutUrl = (String) model.get("logoutUrl");
+                // get from the model
+                Page<InboxItem> inboxItems = (Page<InboxItem>) model.get("inboxItems");
+                String homeUrl = (String) model.get("homeUrl");
+                String searchUrl = (String) model.get("searchUrl");
+                String username = (String) model.get("username");
+                String logoutUrl = (String) model.get("logoutUrl");
 
-        // build the ui
-        DomContent html = SiteLayout.add(
-            "Inboxed | Reader",
-            model,
-            each(
-                inboxedNavigation(homeUrl, searchUrl, username, logoutUrl),
-                div().withClass(Bootstrap.container)
-                // ...existing code (other contents of each, if any)...
-            )
-        );
+                // build the ui
+                DomContent html = SiteLayout.add(
+                                "Inboxed | Reader",
+                                model,
+                                each(
+                                                inboxedNavigation(homeUrl, searchUrl, username, logoutUrl),
+                                                div().withClasses(container_fluid).with(
+                                                                actionBar(),
+                                                                hr(),
+                                                                div().withClass(row).with(
+                                                                                div().withClass(col_2).with(
+                                                                                                readerNavigation()),
+                                                                                div().withClass(col).with(
+                                                                                                inboxItems(inboxItems))))));
 
-        // output the html
-        setResponseContentType(request, response);
-        html.render(IndentedHtml.into(response.getWriter()));
-        
-    }
+                // output the html
+                setResponseContentType(request, response);
+                html.render(IndentedHtml.into(response.getWriter()));
+
+        }
+
+        private static DomContent inboxItems(Page<InboxItem> inboxItems) {
+                return div().withId("inbox-items").with(
+                        h3().withText("All items"),
+                        each(
+                                inboxItems.getContent(),
+                                item -> div().withClass(
+                                                card)
+                                                .with(
+                                                                div().withClass(card_body)
+                                                                                .with(
+                                                                                                h5(item.title()),
+                                                                                                p(item.summary())))));
+        }
+
+        private static NavTag readerNavigation() {
+                return nav()
+                                .with(
+                                                ul().withClasses(
+                                                                nav,
+                                                                flex_column)
+                                                                .with(
+
+                                                                                li().withClass(nav_item)
+                                                                                                .with(button()
+                                                                                                                .withClasses(btn,
+                                                                                                                                btn_primary,
+                                                                                                                                mx_3,
+                                                                                                                                mb_4)
+                                                                                                                .withType("button")
+                                                                                                                .withText("Subscribe")),
+
+                                                                                li().withClass(nav_item)
+                                                                                                .with(a("Home").withClasses(
+                                                                                                                nav_link,
+                                                                                                                active)
+                                                                                                                .withHref("#")),
+
+                                                                                li().withClass(nav_item)
+                                                                                                .with(a("All Items (23)")
+                                                                                                                .withClasses(nav_link,
+                                                                                                                                active)
+                                                                                                                .withHref("#")),
+
+                                                                                li().withClass(nav_item)
+                                                                                                .with(a("Subscriptions")
+                                                                                                                .withClass(nav_link)
+                                                                                                                .withHref("#")))
+
+                                );
+        }
+
+        private static HeaderTag actionBar() {
+
+                return header().withClasses(row).with(
+
+                                div().withClass(col_2).with(
+                                                h2("Reader").withClasses(px_3)),
+
+                                div().withClass(col).with(
+
+                                                button().withClasses(btn, btn_secondary)
+                                                                .withType("button")
+                                                                .with(span().withClasses("bi", "bi-arrow-clockwise")),
+
+                                                div().withClass(btn_group).with(
+                                                                button().withClasses(btn, btn_secondary, btn_sm,
+                                                                                dropdown_toggle)
+                                                                                .attr("data-bs-toggle", "dropdown")
+                                                                                .withType("button")
+                                                                                .withText("23 new items"),
+                                                                ul().withClasses(dropdown_menu).with(
+                                                                                li().with(
+                                                                                                a().withClasses(dropdown_item)
+                                                                                                                .withHref("#")
+                                                                                                                .withText("All items")))),
+
+                                                div().withClass(btn_group).with(
+                                                                button().withClasses(btn, btn_secondary, btn_sm,
+                                                                                dropdown_toggle)
+                                                                                .attr("data-bs-toggle", "dropdown")
+                                                                                .withType("button")
+                                                                                .withText("Refresh"),
+                                                                ul().withClasses(dropdown_menu).with(
+                                                                                li().with(
+                                                                                                a().withClasses(dropdown_item)
+                                                                                                                .withHref("#")
+                                                                                                                .withText("All items")))),
+
+                                                div().withClass(btn_group).with(
+                                                                button().withClasses(btn, btn_secondary, btn_sm,
+                                                                                dropdown_toggle)
+                                                                                .attr("data-bs-toggle", "dropdown")
+                                                                                .withType("button")
+                                                                                .withText("Mark all as read"),
+                                                                ul().withClasses(dropdown_menu).with(
+                                                                                li().with(
+                                                                                                a().withClasses(dropdown_item)
+                                                                                                                .withHref("#")
+                                                                                                                .withText("All items")))),
+
+                                                div().withClass(btn_group).with(
+                                                                button().withClasses(btn, btn_secondary, btn_sm,
+                                                                                dropdown_toggle)
+                                                                                .attr("data-bs-toggle", "dropdown")
+                                                                                .withType("button")
+                                                                                .withText("View settings"),
+                                                                ul().withClasses(dropdown_menu).with(
+                                                                                li().with(
+                                                                                                a().withClasses(dropdown_item)
+                                                                                                                .withHref("#")
+                                                                                                                .withText("All items")))),
+
+                                                button().withClasses(btn, btn_secondary)
+                                                                .withType("button")
+                                                                .with(span().withClasses("bi", "bi-card-list")),
+
+                                                button().withClasses(btn, btn_secondary)
+                                                                .withType("button")
+                                                                .with(span().withClasses("bi", "bi-card-text")),
+
+                                                button().withClasses(btn, btn_secondary)
+                                                                .withType("button")
+                                                                .with(span().withClasses("bi", "bi-chevron-up")),
+
+                                                button().withClasses(btn, btn_secondary)
+                                                                .withType("button")
+                                                                .with(span().withClasses("bi", "bi-chevron-down")),
+
+                                                button().withClasses(btn, btn_secondary)
+                                                                .withType("button")
+                                                                .with(span().withClasses("bi", "bi-gear"))
+
+                                ));
+
+        }
 
 }
--- src/main/java/news/inboxed/app/web/site/Navigation.java
+++ src/main/java/news/inboxed/app/web/site/Navigation.java
@@ -1,51 +1,7 @@
 package news.inboxed.app.web.site;
 
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.active;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.bg_body_tertiary;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.bg_primary;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.bg_primary_subtle;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.btn;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.btn_outline_secondary;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.btn_secondary;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.btn_success;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.collapse;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.container_fluid;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.d_flex;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.d_inline_flex;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.d_lg_flex;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.dropdown;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.dropdown_divider;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.dropdown_item;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.dropdown_menu;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.dropdown_toggle;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.form_control;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.justify_content_between;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.justify_content_center;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.justify_content_lg_between;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.mb_0;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.mb_2;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.mb_lg_0;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.me_2;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.me_auto;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.nav_item;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.nav_link;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.navbar;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.navbar_brand;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.navbar_collapse;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.navbar_expand_lg;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.navbar_nav;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.navbar_toggler;
-import static dev.rebelcraft.j2html.bootstrap.Bootstrap.navbar_toggler_icon;
-import static j2html.TagCreator.a;
-import static j2html.TagCreator.button;
-import static j2html.TagCreator.div;
-import static j2html.TagCreator.form;
-import static j2html.TagCreator.hr;
-import static j2html.TagCreator.input;
-import static j2html.TagCreator.li;
-import static j2html.TagCreator.nav;
-import static j2html.TagCreator.span;
-import static j2html.TagCreator.ul;
+import static dev.rebelcraft.j2html.bootstrap.Bootstrap.*;
+import static j2html.TagCreator.*;
 
 import j2html.tags.specialized.NavTag;
 
@@ -53,55 +9,54 @@ public class Navigation {
 
     public static NavTag inboxedNavigation(String homeUrl, String searchUrl, String username, String logoutUrl) {
 
-        return nav().withClasses(navbar, navbar_expand_lg, bg_primary_subtle).with(
+        return nav().withClasses(navbar, navbar_expand_lg, bg_primary_subtle, mb_4).with(
 
-            div().withClasses(container_fluid).with(
+                div().withClasses(container_fluid).with(
 
-                a().withClasses(navbar_brand).withText("Inboxed").withHref(homeUrl),
+                        a().withClasses(navbar_brand, px_3).withText("Inboxed").withHref(homeUrl),
 
-                button().withClass(navbar_toggler).withType("button")
-                .attr("data-bs-toggle", "collapse").attr("data-bs-target","#navbarToggler")
-                .attr("aria-controls", "navbarToggler").attr("aria-expanded", "false")
-                .attr("aria-label", "Toggle Navigation").with(
-                    span().withClass(navbar_toggler_icon)
-                ),
+                        button().withClass(navbar_toggler).withType("button")
+                                .attr("data-bs-toggle", "collapse").attr("data-bs-target", "#navbarToggler")
+                                .attr("aria-controls", "navbarToggler").attr("aria-expanded", "false")
+                                .attr("aria-label", "Toggle Navigation").with(
+                                        span().withClass(navbar_toggler_icon)),
 
-                div().withClasses(collapse, navbar_collapse, d_lg_flex, justify_content_lg_between).withId("navbarToggler").with(
+                        div().withClasses(collapse, navbar_collapse, d_lg_flex, justify_content_lg_between)
+                                .withId("navbarToggler").with(
 
-                    div(),
+                                        div(),
 
-                    form().withClasses(d_inline_flex, mb_0).with(
+                                        form().withClasses(d_inline_flex, mb_0).with(
 
-                        input().withClasses(form_control, me_2).withType("search"),
-                        button().withType("submit").withClasses(btn, btn_outline_secondary).withText("Search")
+                                                input().withClasses(form_control, me_2).withType("search"),
+                                                button().withType("submit").withClasses(btn, btn_outline_secondary)
+                                                        .with(span().withClasses("bi", "bi-search"))
 
-                    ),
+                                        ),
 
-                    ul().withClass(navbar_nav).with(
+                                        ul().withClass(navbar_nav).with(
 
-                        li().withClasses(nav_item, dropdown).with(
-                            a().withClasses(nav_link, dropdown_toggle).withText("my@email.com")
-                            .withHref("#")
-                                .attr("data-bs-toggle", "dropdown")
-                                .attr("role", "button")
-                                .attr("aria-expanded", "false"),
+                                                li().withClasses(nav_item, dropdown).with(
+                                                        a().withClasses(nav_link, dropdown_toggle)
+                                                                .withText("my@email.com")
+                                                                .withHref("#")
+                                                                .attr("data-bs-toggle", "dropdown")
+                                                                .attr("role", "button")
+                                                                .attr("aria-expanded", "false"),
 
-                            ul().withClasses(dropdown_menu).with(
-                                li().with(
-                                    a().withClasses(dropdown_item).withHref(logoutUrl).withText("Logout")
+                                                        ul().withClasses(dropdown_menu).with(
+                                                                li().with(
+                                                                        a().withClasses(dropdown_item)
+                                                                                .withHref(logoutUrl)
+                                                                                .withText("Logout"))))
+
+                                        )
+
                                 )
-                            )
-                        )
-                            
-                        
-
-                    )
 
                 )
 
-            )
-            
-               );
+        );
     }
 
 }
